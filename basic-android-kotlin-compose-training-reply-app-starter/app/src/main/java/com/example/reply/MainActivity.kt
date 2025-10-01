c5555ca82e082ec929cb6bd354c84cd22fa25f7e
ngoctaidev <ngocthang8079@gmail.com>
2025-10-01 21:47:33 +0700

    replyapp windowSize

--- basic-android-kotlin-compose-training-reply-app-starter/app/src/main/java/com/example/reply/MainActivity.kt
+++ basic-android-kotlin-compose-training-reply-app-starter/app/src/main/java/com/example/reply/MainActivity.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.reply
 
 import android.os.Bundle
@@ -32,9 +17,13 @@ import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.reply.ui.ReplyApp
 import com.example.reply.ui.theme.ReplyTheme
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 
 class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
@@ -50,7 +39,10 @@ class MainActivity : ComponentActivity() {
                                 .calculateEndPadding(layoutDirection)
                         )
                 ) {
-                    ReplyApp()
+                    val windowSize = calculateWindowSizeClass(this)
+                    ReplyApp(
+                        windowSize = windowSize.widthSizeClass
+                    )
                 }
             }
         }
@@ -62,7 +54,9 @@ class MainActivity : ComponentActivity() {
 fun ReplyAppCompactPreview() {
     ReplyTheme {
         Surface {
-            ReplyApp()
+            ReplyApp(
+                windowSize = WindowWidthSizeClass.Compact,
+            )
         }
     }
 }
---
+++ basic-android-kotlin-compose-training-reply-app-starter/app/src/main/java/com/example/reply/ui/utils/WindowStateUtils.kt
@@ -0,0 +1,7 @@
+package com.example.reply.ui.utils
+// Enum này định nghĩa 3 loại thanh điều hướng mà chúng ta sẽ sử dụng.
+enum class ReplyNavigationType {
+    BOTTOM_NAVIGATION,      // Thanh điều hướng dưới đáy
+    NAVIGATION_RAIL,        // Thanh điều hướng cạnh
+    PERMANENT_NAVIGATION_DRAWER // Ngăn điều hướng cố định bên trái
+}
